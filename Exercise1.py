8c90cba6c8d0acda4fa3b279020b22a584ca5414
Eva Theodoridou <e.theodoridou1@newcastle.ac.uk>
2017-05-23 23:41:43 +0100

    Update according to review

--- Exercise1.py
+++ Exercise1.py
@@ -1,49 +1,60 @@
 #!/usr/bin/env python
 
+"""
+Method determines if two strings are permutations of each other
 
+string1, string2: two string variables which will be compared for the purposes of this function
+
+return true if the two strings are permutations of each other, otherwise false
+"""
 def is_permutation(string1, string2) :
-	# check if any of the strings is None
 	if string1 is None or string2 is None:
 		return False
-	# change strings inputted to dictionaries
+	
 	string1_dict = string_to_dict(string1)
 	string2_dict = string_to_dict(string2)
 	
-	# check if the two dictionaries are equal
-	result = dicts_are_equal(string1_dict, string2_dict)
-	
-	return result
+	return dicts_are_equal(string1_dict, string2_dict)
+
+def is_permutation_english(string1, string2) : 
+    if string1 is None or string2 is None:
+        return False
+    
+    string1_dict = string_to_dict_english(string1)
+    string2_dict = string_to_dict_english(string2)
+    
+    return dicts_are_equal(string1_dict, string2_dict)
 	
 def string_to_dict(input_string) :
-	# make sure all characters are lower case
 	lower_case_string = input_string.lower()
 	
 	# create a dictionary where you map each character with a counter of
 	# how many instances of a character exist
-	# if the strings can only include characters from the English alphabet:
-	# initialise the dictionary at this stage with all the letters of the alphabet
-	# and a starting counter value of 0
 	dict = {}
 	
-	# add the characters to the dictionary
 	for character in lower_case_string:
 		if character in dict:
-			value = dict[character]
-			dict[character] = value + 1
+			dict[character] = dict.get(character) + 1
 		else:
 			dict[character] = 1
-			
-	# for the case where only the English alphabet is allowed
-	# modify the for-loop
-	# 		
-	# for character in lower_case_string:
-	# 	if character in dict:
-	# 		value = dict[character]
-	# 		dict[character] = value + 1
-	# 	else:
-	# 		return None
-	
+
 	return dict
+
+def string_to_dict_english(input_string):
+    lower_case_string = input_string.lower()	
+	
+	dict = {}
+	
+	for alphabet in range('a','z'):
+		dict[character] = 0
+	
+	for character in lower_case_string:
+		if character in dict:		
+			dict[character] = dict.get(character) + 1
+    	else:			
+            return None
+    
+    return dict
 
 def dicts_are_equal(dict1, dict2) :
 	
@@ -58,9 +69,9 @@ def dicts_are_equal(dict1, dict2) :
 	return True
 
 def main() :
-	string1 = raw_input('Please insert the first string.')
+	string1 = raw_input('Please insert the first string: ')
 	print
-	string2 = raw_input('What would you like to compare the first string with?')
+	string2 = raw_input('What would you like to compare the first string with: ')
 	print
 	
 	print is_permutation(string1, string2)  
