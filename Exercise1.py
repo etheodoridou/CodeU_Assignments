cd474c72cf761d2c49e3b8fe0c91dc6f49a0bd5a
Eva Theodoridou <e.theodoridou1@newcastle.ac.uk>
2017-06-21 13:26:28 +0100

    Fixed part 1 according to review

--- Exercise1.py
+++ Exercise1.py
@@ -1,77 +1,74 @@
 #!/usr/bin/env python
 
-"""
-Method determines if two strings are permutations of each other
+from string import ascii_lowercase
 
-string1, string2: two string variables which will be compared for the purposes of this function
-
-return true if the two strings are permutations of each other, otherwise false
-"""
 def is_permutation(string1, string2) :
-	if string1 is None or string2 is None:
-		return False
-	
-	string1_dict = string_to_dict(string1)
-	string2_dict = string_to_dict(string2)
-	
-	return dicts_are_equal(string1_dict, string2_dict)
+    """Determines if two strings are permutations of each other.
+    
+    Args:
+        string1: the first string to compare
+        string2: the second string to compare
+        
+    Returns:
+        True if the two strings are permutations of each other, otherwise false
+    """
+    if string1 is None or string2 is None:
+        return False
+    
+    string1_dict = string_to_dict(string1)
+    string2_dict = string_to_dict(string2)
+    
+    return string1_dict == string2_dict
 
-def is_permutation_english(string1, string2) : 
+def is_permutation_english(string1, string2) : 
     if string1 is None or string2 is None:
         return False
     
     string1_dict = string_to_dict_english(string1)
     string2_dict = string_to_dict_english(string2)
     
-    return dicts_are_equal(string1_dict, string2_dict)
-	
+    if string1_dict == {} or string2_dict == {} :
+        return False
+    
+    return string1_dict == string2_dict
+    
 def string_to_dict(input_string) :
-	lower_case_string = input_string.lower()
-	
-	# create a dictionary where you map each character with a counter of
-	# how many instances of a character exist
-	dict = {}
-	
-	for character in lower_case_string:
-		if character in dict:
-			dict[character] = dict.get(character) + 1
-		else:
-			dict[character] = 1
+    lower_case_string = input_string.lower()
+    
+    # create a dictionary where you map each character with a counter of
+    # how many instances of a character exist
+    dictionary = {}
+    
+    for character in lower_case_string:
+        if character in dictionary:
+            dictionary[character] += 1
+        else:
+            dictionary[character] = 1
 
-	return dict
+    return dictionary
 
 def string_to_dict_english(input_string):
-    lower_case_string = input_string.lower()	
-	
-	dict = {}
-	
-	for alphabet in range('a','z'):
-		dict[character] = 0
-	
-	for character in lower_case_string:
-		if character in dict:		
-			dict[character] = dict.get(character) + 1
-    	else:			
-            return None
+    lower_case_string = input_string.lower()    
     
-    return dict
-
-def dicts_are_equal(dict1, dict2) :
-	
-	# check that for each key in one dictionary,
-	# the same key exists in the other dictionary
-	# and the counter values are equal
-	for key in dict1:
-		if key in dict2 and dict2[key] == dict1[key]:
-			continue
-		else:
-			return False
-	return True
+    dictionary = {}
+    
+    for alphabet in ascii_lowercase:
+        dictionary[alphabet] = 0
+    
+    for character in lower_case_string:
+        if character in dictionary:        
+            dictionary[character] = dictionary.get(character) + 1 
+        else:       
+            return {}
+    
+    return dictionary
 
 def main() :
-	string1 = raw_input('Please insert the first string: ')
-	print
-	string2 = raw_input('What would you like to compare the first string with: ')
-	print
-	
-	print is_permutation(string1, string2)  
+    string1 = input('Please insert the first string: ')
+    string2 = input('What would you like to compare the first string with: ') 
+
+    print(is_permutation(string1, string2))
+    print(is_permutation_english(string1, string2))
+    
+if __name__ == "__main__":
+    main()
--- Exercise1_Test.py
+++ Exercise1_Test.py
@@ -5,14 +5,33 @@ import unittest
 
 class Exercise1Test(unittest.TestCase):
 
-  def testBaseCases(self):
-    self.assertEquals(True, Exercise1.is_permutation("", ""))
-    self.assertEquals(False, Exercise1.is_permutation(None, None))
+    def testBaseCases(self):
+        self.assertTrue(Exercise1.is_permutation("", ""))
+        self.assertFalse(Exercise1.is_permutation(None, None))
+        self.assertFalse(Exercise1.is_permutation(None, "Silent"))
+        self.assertFalse(Exercise1.is_permutation("Silent", None))
+        self.assertTrue(Exercise1.is_permutation("a" * 10, "a" * 10))    
+    
+    def testKnownCases(self):
+        self.assertTrue(Exercise1.is_permutation("Triangle", "Integral"))
+        self.assertTrue(Exercise1.is_permutation("Silent", "Listen"))
+        self.assertFalse(Exercise1.is_permutation("Apple", "Pabble"))
+        self.assertTrue(Exercise1.is_permutation("1Hj)w", "W)Jh1"))  
+    
+    def testBaseCases_English(self):
+        self.assertTrue(Exercise1.is_permutation_english("", ""))
+        self.assertFalse(Exercise1.is_permutation_english(None, None))
+        self.assertFalse(Exercise1.is_permutation_english(None, "Silent"))
+        self.assertFalse(Exercise1.is_permutation_english("Silent", None))
+        self.assertTrue(Exercise1.is_permutation("a" * 10, "a" * 10))      
+    
+    def testKnownCases_English(self):
+        self.assertTrue(Exercise1.is_permutation_english("Triangle", "Integral"))
+        self.assertTrue(Exercise1.is_permutation_english("Silent", "Listen"))
+        self.assertFalse(Exercise1.is_permutation_english("Apple", "Pabble"))
 
-  def testKnownCases(self):
-    self.assertEquals(True, Exercise1.is_permutation("Triangle", "Integral"))
-    self.assertEquals(True, Exercise1.is_permutation("Silent", "Listen"))
-    self.assertEquals(False, Exercise1.is_permutation("Apple", "Pabble"))
-
+    def testErrorCases(self):
+        self.assertFalse(Exercise1.is_permutation_english("12345678", "87654321"))
+    
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
