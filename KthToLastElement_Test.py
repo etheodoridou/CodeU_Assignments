8730422bf1b9ae9a958ac6e6b5f0ff6de06ce5f0
Eva Theodoridou <e.theodoridou1@newcastle.ac.uk>
2017-06-21 19:29:07 +0100

    Fixed part 2 according to review

--- KthToLastElement_Test.py
+++ KthToLastElement_Test.py
@@ -1,23 +1,33 @@
 #!/usr/bin/env python
 
-from SimplyLinkedList import SimplyLinkedList
+from linked_list import LinkedList, kth_to_last_element
+import unittest
 
-def main():
-	linked_list = SimplyLinkedList()
-	
-	linked_list.add_first(0)
-	linked_list.add_first(1)
-	linked_list.add_first(2)
-	linked_list.add_first(3)
-	linked_list.add_first(4)
-	linked_list.add_first(5)
-	linked_list.add_first(6)
-	linked_list.add_first(7)
-	linked_list.add_first(8)
-	
-	list_head = linked_list.get_head()
-	
-	linked_list.kth_to_last_element(list_head, 3)
-	
-if __name__ == "__main__":
-	main()
+class KthToLastElement_Test(unittest.TestCase):
+
+    def setUp(self):
+        self.__linked_list = LinkedList()
+        self.__linked_list.add_first(0)
+        self.__linked_list.add_first(1)
+        self.__linked_list.add_first(2)
+        self.__linked_list.add_first(3)
+        self.__linked_list.add_first(4)
+        self.__linked_list.add_first(5)
+        self.__linked_list.add_first(6)
+        self.__linked_list.add_first(7)
+        self.__linked_list.add_first(8)
+        self.__list_head = self.__linked_list.get_head()
+        
+    def testBaseCases(self):
+        self.__max_value = self.__linked_list.size()
+        for i in range(0, self.__max_value):
+            self.assertEqual(kth_to_last_element(self.__list_head, i), i)
+        
+    def testErrorCases(self):
+        with self.assertRaises(ValueError): 
+                kth_to_last_element(self.__list_head, -1)
+                kth_to_last_element(self.__list_head, self.__max_value + 1)
+        
+    
+if __name__ == '__main__':
+    unittest.main()
