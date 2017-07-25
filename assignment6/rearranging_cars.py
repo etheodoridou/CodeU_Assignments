e306e265fc267ca553b162dd5fd43d2c43e4fbcd
Eva Theodoridou <e.theodoridou1@newcastle.ac.uk>
2017-07-25 19:23:48 +0200

    Update

--- assignment6/rearranging_cars.py
+++ assignment6/rearranging_cars.py
@@ -5,13 +5,15 @@ Created on 07 July 2017
 '''
 
 def print_car_moves(initial_car_park, final_car_park):
-    car_moves = rearrange_cars(initial_car_park, final_car_park)
+    rearranging_moves = rearrange_cars(initial_car_park, final_car_park)
     
-    for i in range(len(car_moves)):
-        print(car_moves[i].__str__())
+    for i in rearranging_moves:
+        print(i)
+    print("{} moves in total.".format(len(rearranging_moves)))
 
 def rearrange_cars(initial_car_park, final_car_park):
-    """Rearranges the cars from the initial positions to the end positions.
+    """
+    Rearranges the cars from the initial positions to the end positions.
     
     Args:
         initial_car_park: an array, represents the initial positions of the cars in the parking lot
@@ -24,34 +26,47 @@ def rearrange_cars(initial_car_park, final_car_park):
         raise ValueError("must be the same length: initial_car_park, final_car_park")
     
     EMPTY_SPACE = 0
-    # Dictionary used for O(1) lookups when searching for the index of a value in the car_park
-    car_map = {}
+    CAR_PARK_SIZE = len(initial_car_park)
+    # List used for O(1) lookups when searching for the index of a value in the car_park
+    car_positions = [0] * CAR_PARK_SIZE
     moves = []
     
-    for i in range(len(initial_car_park)):
-        car_map[initial_car_park[i]] = i
+    for i in range(CAR_PARK_SIZE):
+        car_at_i = initial_car_park[i]
+        car_positions[car_at_i] = i
     
-    for space in range(len(initial_car_park)):
-        if initial_car_park[space] != final_car_park[space] and initial_car_park[space] != EMPTY_SPACE:
-            empty_space_index = car_map[EMPTY_SPACE]
-            move_car(initial_car_park, car_map, space, empty_space_index, moves)
-            if initial_car_park != final_car_park:
-                end_value_index = car_map[final_car_park[space]]
-                move_car(initial_car_park, car_map, end_value_index, space, moves)
-
+    for space in range(CAR_PARK_SIZE):
+        if initial_car_park[space] != final_car_park[space]:
+            if initial_car_park[space] != EMPTY_SPACE:
+                empty_space_index = car_positions[EMPTY_SPACE]
+                new_move = move_car(initial_car_park, car_positions, space, empty_space_index)
+                moves.append(new_move)
+        if initial_car_park[space] != final_car_park[space]:
+            end_value_index = car_positions[final_car_park[space]]
+            new_move = move_car(initial_car_park, car_positions, end_value_index, space)
+            moves.append(new_move)
+     
     return moves
 
-def move_car(car_park, car_map, _from, _to, moves):
-    # Swap the two cars in the parking lot
-    swap(car_park, _from, _to)
-    # Update car map to reflect parking lot
-    swap(car_map, car_park[_from], car_park[_to])
-    moves.append(Move(_from, _to))
-
-def swap(array, _from, _to):
-    temp = array[_from]
-    array[_from] = array[_to]
-    array[_to] = temp
+def move_car(car_park, car_map, _from, _to):
+    """
+    Swaps the positions of two cars in the car park and updates the map of that records their positions.
+    
+    Args:
+        car_park: an array, represents the positions of the cars in the parking lot
+        car_map: a list, records the index of each car in the car park to improve performance
+        _from: an integer, represents the source index of the car in the car park
+        _to: an integer, represents the index in the car park to which the car will be moved
+        
+    Returns:
+        a Move object that represents the move taken to change the position of the car
+    """
+    src_pos = car_park[_from]
+    dest_pos = car_park[_to]
+    car_park[_from], car_park[_to] = car_park[_to], car_park[_from]
+    car_map[src_pos], car_map[dest_pos] = car_map[dest_pos], car_map[src_pos]
+    
+    return Move(_from, _to)
    
 class Move:
     def __init__(self, origin, destination):
@@ -64,7 +79,11 @@ class Move:
     def get_destination(self):
         return self.destination
     
+    def __eq__(self, other):
+        return (self.origin == other.origin and
+                self.destination == other.destination)
+        
     def __str__(self):
-        return "Move from " + str(self.get_origin()) + " to " + str(self.get_destination())
+        return "Move car from {} to {}".format(self.get_origin(), self.get_destination())
     
         
